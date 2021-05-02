c1573345b220bc6b6445524b61ca121ddfa4d8a3
Rad Eshghi <64996038+Rad-tech-spec@users.noreply.github.com>
2021-05-02 13:10:55 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,3 @@
-/********************************************************************************* 
-* BTI425 – Assignment 04
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this 
-* assignment has been copied manually or electronically from any other source (including web sites) or 
-* distributed to other students.
-* Name: Rad Eshghi Student ID: 123348195 Date: April / 7 / 2021
-* ********************************************************************************/
-
 import { Component } from '@angular/core';
 
 @Component({
